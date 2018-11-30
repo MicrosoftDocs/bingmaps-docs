8595ed96d541a2f726db31206eb694f2b9550e0d
Christopher French <v-chrfr@microsoft.com>
2018-11-30 10:26:37 -0800

    python stuff / data

---
+++ BingMaps/msdn_migration/fix_https_links.py
@@ -0,0 +1,54 @@
+from filemap import FileMap
+
+from os.path import expanduser as ospath
+
+data_file = 'msdn_migration/bad_http_links.csv'
+
+mapper = FileMap(ignore_dir=['msdn_migration'])
+
+
+def get_http_data():
+    with open(data_file, 'r', encoding='utf-8') as f:
+        for n, line in enumerate(f.readlines()):
+            if n > 0:
+                yield line.split(',')
+            
+
+broken_url_head = 'The following referenced link ['
+broken_url_tail = '] supports HTTPS. Please change the link to use HTTPS.\n'
+
+N = len(broken_url_head)
+M = len(broken_url_tail)
+    
+for row in get_http_data():
+
+    # exit(1)
+    strip_head = 'https://github.com/MicrosoftDocs/bingmaps-docs/blob/master/BingMaps/'
+    
+
+    test_url_raw = row[-1]
+    
+
+    if broken_url_head in test_url_raw:
+        source_file_parts = row[1].strip(strip_head).split('/')
+        length = len(test_url_raw)
+        link = test_url_raw[N+1: length - M]
+
+        source_file_parts[-1] += '.md'
+
+        path = mapper.get_path(*source_file_parts)
+
+        html = None
+        with open(path, 'r', encoding='utf-8') as f:
+            html = f.read()
+
+
+        if html:
+            new_link = link.replace('http://', 'https://')
+            new_html = html.replace(link, new_link)
+            print(f'link:\n\t{link}\nnew link:\n\t{new_link}')
+            assert(html != new_html)
+
+            with open(path, 'w', encoding='utf-8') as f:
+                f.write(new_html)
+                print(f'replaced: {path}')
